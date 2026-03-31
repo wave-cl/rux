e266ad5c292e3cb2fe57b175fdaf726aa026835d
c <wave-cl@pm.me>
2026-03-30 22:05:41 -0700

    Bump version to 0.3.0

--- test.sh
+++ test.sh
@@ -107,7 +107,7 @@ OUTPUT=$( { sleep 3; \
     -no-reboot -monitor none -m 128M 2>&1 || true )
 
 # Boot
-check "boot banner"             "rux 0.2.0 (x86_64)"
+check "boot banner"             "rux 0.3.0 (x86_64)"
 check "kernel page tables"      "CR3 switched to kernel page tables"
 check "cpio unpacked"           "cpio: unpacked"
 check "procfs mounted"          "procfs mounted at /proc"
@@ -115,7 +115,7 @@ check "exec init"               "exec /sbin/init"
 check "shell prompt"            "/ # "
 
 # Core commands
-check "uname"                   "rux rux 0.2.0"
+check "uname"                   "rux rux 0.3.0"
 check "cat /etc/passwd"         "root:x:0:0:root:/root:/bin/sh"
 check "cat /etc/os-release"     "NAME=\"rux\""
 check "whoami"                  "root"
@@ -279,7 +279,7 @@ OUTPUT=$( { sleep 8; \
     -semihosting -no-reboot -m 128M 2>&1 || true )
 
 # Boot
-check "boot banner"             "rux 0.2.0 (aarch64)"
+check "boot banner"             "rux 0.3.0 (aarch64)"
 check "MMU enabled"             "MMU enabled"
 check "cpio unpacked"           "cpio: unpacked"
 check "procfs mounted"          "procfs mounted at /proc"
@@ -287,7 +287,7 @@ check "exec init"               "exec /sbin/init"
 check "shell prompt"            "/ # "
 
 # Core commands
-check "uname"                   "rux rux 0.2.0"
+check "uname"                   "rux rux 0.3.0"
 check "cat /etc/passwd"         "root:x:0:0:root:/root:/bin/sh"
 check "cat /etc/os-release"     "NAME=\"rux\""
 check "whoami"                  "root"
